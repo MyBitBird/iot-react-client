2033e9e1389a170d7004b62a180c1cc97270d7a5
meisam <meisammalekzadeh@gmail.com>
2018-10-28 23:41:44 +0330

    dynamic routing based on authentication

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Switch,Route,Redirect,withRouter} from 'react-router-dom'
 import Dashboard from './containers/Dashboard/Dashboard';
 import Axios from 'axios';
 import * as authAction from './store/actions/auth'
+import {connect} from 'react-redux'
 
 class App extends Component {
   
@@ -14,16 +15,26 @@ class App extends Component {
 
     if(localStorage.getItem('token')!=null)
         authAction.checkToken();
+
+    const routes = this.props.isAuthenticated ? 
+      <Switch>
+        <Route path="/" component={Dashboard} />
+      </Switch>
+      :
+      <Switch>
+        <Route path="/" component={Auth} />
+      </Switch>
     
     return (
       <Layout>
-        <Switch>
-          <Route path="/Dashboard" component={Dashboard} />
-          <Route path="/" component={Auth} />
-        </Switch>
+        {routes}
       </Layout>
     );
   }
 }
 
-export default withRouter(App);
+const mapStateToProps = state=>{
+  return { isAuthenticated : state.auth.isAuthenticated}
+}
+
+export default withRouter(connect(mapStateToProps)(App));
