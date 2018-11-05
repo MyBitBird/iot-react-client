f405faca9b0296e42d4a4e54e9fd0ca92aa0c287
meisam <meisammalekzadeh@gmail.com>
2018-11-06 00:01:59 +0330

    add some service resux functions

--- src/store/actions/service.js
+++ src/store/actions/service.js
@@ -17,3 +17,48 @@ export const getAll = ()=>{
         })
     }
 }
+
+export const onFieldChanged = (field, value) => {
+    return {
+        type: actionTypes.SERVICE_FIELD_CHANGE,
+        field: field,
+        value: value
+    }
+}
+
+export const newPropertyHandler = ()=>{
+    return {
+        type: actionTypes.SERVICE_ADD_NEW_PROPERTIES
+    }
+}
+
+export const removePropertyHandler = (index)=>{
+    return{
+        type: actionTypes.SERVICE_DELETE_NEW_PROPERTIES,
+        index: index
+
+    }
+}
+
+export const onPropertyTitleChanged = (index,value)=>{
+    return{
+        type: actionTypes.SERVICE_PROPERTY_TITLE_CHANGED,
+        index: index,
+        value: value
+    }
+}
+
+export const onPropertyCodeChanged = (index, value) => {
+    return {
+        type: actionTypes.SERVICE_PROPERTY_CODE_CHANGED,
+        index: index,
+        value: value
+    }
+}
+
+
+/*export const addService=(props)=>{
+    return dispatch=>{
+        axios.post('/services',)
+    }
+}*/
--- src/store/reducers/service.js
+++ src/store/reducers/service.js
@@ -1,7 +1,10 @@
 import * as actionTypes from '../actions/actionTypes'
 
 const initState={
-    services: null
+    services: null,
+    title:'',
+    serviceProperties: []
+    
 }
 
 const reducer = (state = initState,action)=>{
@@ -10,8 +13,26 @@ const reducer = (state = initState,action)=>{
     {
         case actionTypes.SERVICE_FIELD_CHANGE:
             return { ...state, [action.field]: action.value }
+            
         case actionTypes.SERVICE_LOAD_SERVICE:
             return { ...state, services: action.services }
+
+        case actionTypes.SERVICE_ADD_NEW_PROPERTIES:
+            return {...state,serviceProperties: [...state.serviceProperties].concat({title:'',code:''})}
+
+        case actionTypes.SERVICE_DELETE_NEW_PROPERTIES:
+            return {...state,serviceProperties:[...state.serviceProperties].filter((x,index)=> index!=action.index)}
+
+        case actionTypes.SERVICE_PROPERTY_TITLE_CHANGED:
+            const titleServiceProperties = [...state.serviceProperties];
+            titleServiceProperties.splice(action.index, 1, { title: action.value, code: titleServiceProperties[action.index].code})
+            return { ...state, serviceProperties: titleServiceProperties }
+
+        case actionTypes.SERVICE_PROPERTY_CODE_CHANGED:
+            const codeServiceProperties = [...state.serviceProperties];
+            codeServiceProperties.splice(action.index, 1, { code: action.value, title: codeServiceProperties[action.index].title })
+            return { ...state, serviceProperties: codeServiceProperties }
+
         default:
             return state; 
     }
