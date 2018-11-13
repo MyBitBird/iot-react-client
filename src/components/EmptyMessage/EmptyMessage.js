204c9839c5011b7ea1b780f85c4a261f5ed420f1
meisam <meisammalekzadeh@gmail.com>
2018-11-13 21:18:58 +0330

    make empty message ui a bit better!

--- src/components/EmptyMessage/EmptyMessage.js
+++ src/components/EmptyMessage/EmptyMessage.js
@@ -4,9 +4,13 @@ import { Paper } from '@material-ui/core';
 const EmptyMessage = (props)=>{
 
     const style={
-        padding:10,
+        padding:15,
         width:'80%',
-        margin: 'auto'
+        margin: '15px auto',
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#888888'
     }
 
     return(
