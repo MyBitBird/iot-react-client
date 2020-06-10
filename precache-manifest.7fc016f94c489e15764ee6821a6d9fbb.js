87ebcde1ead706eeea2ddaf1201b53e45fc5cc8d
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-10 13:44:50 +0430

    Updates

---
+++ precache-manifest.7fc016f94c489e15764ee6821a6d9fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "de159a56f1fd1f97737e",
+    "url": "/iot-react-client/static/css/main.1423b386.chunk.css"
+  },
+  {
+    "revision": "de159a56f1fd1f97737e",
+    "url": "/iot-react-client/static/js/main.de159a56.chunk.js"
+  },
+  {
+    "revision": "c85224991a94ef840446",
+    "url": "/iot-react-client/static/js/1.c8522499.chunk.js"
+  },
+  {
+    "revision": "4dd8f0250879f5e9d62b",
+    "url": "/iot-react-client/static/js/runtime~main.4dd8f025.js"
+  },
+  {
+    "revision": "b83e038b3dffdc1af3bf389b14fecd1e",
+    "url": "/iot-react-client/static/media/bitbird.b83e038b.png"
+  },
+  {
+    "revision": "ad38b822fbb4382e38b5e5e464f15766",
+    "url": "/iot-react-client/index.html"
+  }
+];
