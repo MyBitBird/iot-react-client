ba3041f2359b99a53e38b3120ab0e5979f4f1e0d
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-10 23:39:06 +0430

    makre servicedetails more responsive

--- src/components/Service/ServiceDetails/ServiceDetails.js
+++ src/components/Service/ServiceDetails/ServiceDetails.js
@@ -1,46 +1,54 @@
-import React, { Component } from 'react';
-import { Paper, Typography, Chip, Avatar } from '@material-ui/core';
-import * as serviceActions from '../../../store/actions/service'
-import { connect } from 'react-redux'
-import classes from './ServiceDetails.css'
-import Aux from '../../../utils/Auxilary';
+import React, { Component } from "react";
+import { Paper, Typography, Chip, Avatar, Grid } from "@material-ui/core";
+import * as serviceActions from "../../../store/actions/service";
+import { connect } from "react-redux";
+import classes from "./ServiceDetails.css";
+import Aux from "../../../utils/Auxilary";
 
 class ServiceDetails extends Component {
-    componentDidMount() {
-        this.props.onGetServiceDetails(this.props.match.params.id)
-    }
+  componentDidMount() {
+    this.props.onGetServiceDetails(this.props.match.params.id);
+  }
 
-    render() {
+  render() {
+    return (
+      <Grid container>
+        <Grid md={4} xs={false} />
+        <Grid md={4} xs={12}>
+          <Paper className={classes.container}>
+            <Typography variant="h5" component="h3">
+              {this.props.title}
+            </Typography>
+            {this.props.match.params.id}
 
-        return (
-            <Paper className={classes.container}>
-                <Typography variant="h5" component="h3">
-                    {this.props.title}
-                </Typography>
-                {this.props.match.params.id}
-
-                <div className={classes.itemDetails}>
-                    {this.props.serviceProperties.map(
-                        (item, index) => <Chip
-                            avatar={<Avatar>{index + 1}</Avatar>}
-                            label={`${item.title} - ${item.code}`}
-                            color="secondary"
-                            className={classes.chips}
-
-                        />
-                    )}
-                </div>
-            </Paper>
-
-
-        )
-    }
+            <div className={classes.itemDetails}>
+              {this.props.serviceProperties.map((item, index) => (
+                <Chip
+                  avatar={<Avatar>{index + 1}</Avatar>}
+                  label={`${item.title} - ${item.code}`}
+                  color="secondary"
+                  className={classes.chips}
+                />
+              ))}
+            </div>
+          </Paper>
+        </Grid>
+        <Grid md={4} xs={false} />
+      </Grid>
+    );
+  }
 }
 
-const mapStateToProps = state => ({ ...state.service })
+const mapStateToProps = (state) => ({ ...state.service });
 
-const mapDispatchToProps = dispatch => {
-    return { onGetServiceDetails: (id) => dispatch(serviceActions.getServiceDetailsById(id)) }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onGetServiceDetails: (id) =>
+      dispatch(serviceActions.getServiceDetailsById(id)),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(ServiceDetails);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ServiceDetails);
